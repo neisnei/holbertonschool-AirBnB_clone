41655e1461ee887046b7b6fe20f5b36dc4299427
Pepesaur96 <jjvazquez96@gmail.com>
2023-10-30 14:03:53 -0400

    Kwargs update for init function

--- models/base_model.py
+++ models/base_model.py
@@ -8,15 +8,28 @@ import models
 class BaseModel:
     """This class defines all common attributes/methods for other classes"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """This method initializes a BaseModel instance
+        Args:
+            *args: unused
+            **kwargs: key/value pairs used to create instance
         attributes:
             id: unique id for each instance
             created_at: creation date
             updated_at: date of last update"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                elif key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.strptime(value,
+                                                         '%Y-%m-%dT%H:%M:%S.%f'))
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """This method returns a string representation of a BaseModel instance"""
