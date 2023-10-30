4582e6f8934fb2682861906be4244c9c89d30f86
Pepesaur96 <jjvazquez96@gmail.com>
2023-10-30 13:40:13 -0400

    documentation fix for functions

--- models/base_model.py
+++ models/base_model.py
@@ -9,18 +9,28 @@ class BaseModel:
     """This class defines all common attributes/methods for other classes"""
 
     def __init__(self):
+        """This method initializes a BaseModel instance
+        attributes:
+            id: unique id for each instance
+            created_at: creation date
+            updated_at: date of last update"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
     def __str__(self):
+        """This method returns a string representation of a BaseModel instance"""
         return "[{}] ({}) {}".format(self.__class__.__name__,
                                      self.id, self.__dict__)
 
     def save(self):
+        """This method updates the public instance attribute updated_at
+        with the current datetime"""
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        """This method returns a dictionary containing all keys/values
+        of __dict__ of the instance"""
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
         obj_dict['created_at'] = self.created_at.isoformat()
