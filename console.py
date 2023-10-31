f5557360f764df1765be836998046408ea27bb99
Pepesaur96 <jjvazquez96@gmail.com>
2023-10-31 15:15:32 -0400

    console basics. args changed to line

--- console.py
+++ console.py
@@ -9,12 +9,13 @@ class HBNBCommand(cmd.Cmd):
 
     prompt = '(hbnb) '
 
-    def do_quit(self, args):
+    def do_quit(self, line):
         """Quit command to exit the program."""
         return True
 
-    def do_EOF(self, args):
+    def do_EOF(self, line):
         """EOF command to exit the program."""
+        print()
         return True
 
     def emptyline(self):
